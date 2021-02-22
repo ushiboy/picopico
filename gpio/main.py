7c16f62e05ac32363d1467ce2a67145462c5a46e
ushiboy <ushiboy.dev@gmail.com>
2021-02-22 22:47:43 +0900

    fix spec

--- gpio/main.py
+++ gpio/main.py
@@ -1,19 +1,9 @@
 from machine import Pin
-import time
-import _thread
 
 led = Pin(16, Pin.OUT)
 
 def pressed(pin):
-    print("pressed")
-    print(pin.irq().flags())
+    led.toggle()
 
 btn = Pin(17, Pin.IN)
 btn.irq(pressed, Pin.IRQ_FALLING)
-
-def th1():
-    while True:
-        led.value(1 if led.value() == 0 else 0)
-        time.sleep(2)
-
-_thread.start_new_thread(th1, ())
