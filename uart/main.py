75c4ee85468bfe9ccd485e5dae3fd3b43db10cdd
ushiboy <ushiboy.dev@gmail.com>
2021-03-01 22:38:32 +0900

    change uart

--- uart/main.py
+++ uart/main.py
@@ -1,4 +1,13 @@
 from machine import UART
+import time
 
 uart = UART(0, 115200)
 uart.write('abc\n')
+
+while True:
+    buf = b''
+    while uart.any() > 0:
+        buf += uart.read(1)
+    if len(buf) > 0:
+        print(buf)
+    time.sleep_ms(100)
