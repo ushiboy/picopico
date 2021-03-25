42666955173be7d1675a0d5b5940ee25c7084488
ushiboy <ushiboy.dev@gmail.com>
2021-03-25 22:31:22 +0900

    add AQM1602A sample

--- i2c/main.py
+++ i2c/main.py
@@ -2,4 +2,26 @@ from machine import Pin, I2C
 import time
 
 i2c = I2C(0, scl=Pin(21), sda=Pin(20))
-print(i2c.scan())
+addr = i2c.scan()[0]
+
+WRITE_COMMAND = 0x00
+WRITE_DATA = 0x40
+
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x38')
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x39')
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x14')
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x73')
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x56')
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x6c')
+time.sleep(0.2)
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x02')
+time.sleep(0.2)
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x01')
+time.sleep(0.2)
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x0f')
+time.sleep(0.2)
+
+i2c.writeto_mem(addr, WRITE_COMMAND, b'\x38')
+i2c.writeto_mem(addr, WRITE_DATA, bytes([ord('A')]))
+i2c.writeto_mem(addr, WRITE_DATA, bytes([ord('B')]))
+i2c.writeto_mem(addr, WRITE_DATA, bytes([ord('C')]))
